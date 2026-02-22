735e4246ee799e517d38446db313fb0a75c088de
hukasigi <hukasigiteya2@gmail.com>
2026-02-22 11:52:41 +0900

    fix:Variable_name

--- lib/SpeedPID.h
+++ lib/SpeedPID.h
@@ -11,7 +11,8 @@ class SpeedPID {
 
     public:
         SpeedPID(double kp, double ki, double kd, double out_min, double out_max, double dt)
-            : kp(kp), ki(ki), kd(kd), out_min(out_min), out_max(out_max), dt(dt), prev_error(0.0), prev_delta_error(0.0), output(0.0) {}
+            : kp(kp), ki(ki), kd(kd), out_min(out_min), out_max(out_max), dt(dt), prev_error(0.0), prev_delta_error(0.0),
+              output(0.0) {}
 
         double update(double target_rpm, double now_rpm) {
             // 目標値 - 実測値でエラーを出す
@@ -19,20 +20,20 @@ class SpeedPID {
 
             // 増分型 PID
             const double delta_error = error - prev_error;
-            const double deriv = delta_error - prev_delta_error;
-            const double du = kp * delta_error + ki * error * dt + kd * deriv;
+            const double deriv       = delta_error - prev_delta_error;
+            const double du          = kp * delta_error + ki * error * dt + kd * deriv;
             output += du;
             output = constrain(output, out_min, out_max);
 
-            prev_error = error;
+            prev_error       = error;
             prev_delta_error = delta_error;
 
             return output;
         }
 
         void reset() {
-            prev_error = 0.;
+            prev_error       = 0.;
             prev_delta_error = 0.;
-            output = 0.;
+            output           = 0.;
         }
 };
